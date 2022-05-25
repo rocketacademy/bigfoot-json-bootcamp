108454a1fcf6ef62757227a43cdfaa8fa46193e5
neokaiyuan <kai@rocketacademy.co>
2022-05-25 22:41:37 +0800

    Solution base

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+import cors from "cors";
 import express from "express";
 import { getSightings } from "./utils.js";
 
 const PORT = 3000;
 const app = express();
 
+// Enable CORS access to this server
+app.use(cors());
+
 app.get("/", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  res.json(sightings[req.params.sightingIndex]);
 });
 
 app.listen(PORT, () => {
