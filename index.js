86a0c874fd5589b272008fc0b1d36f5a28f6de36
mafiageek <anton_kho@msn.com>
2023-01-03 22:53:28 +0800

    added route

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const foundIndex = req.params.sightingIndex;
+  const sightings = await getSightings();
+  res.json(sightings[foundIndex]);
 });
 
 app.listen(PORT, () => {
