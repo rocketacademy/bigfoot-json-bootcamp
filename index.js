febe6ca1262e5c7e286c1a4977c5d92b8795d6ca
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-01-06 16:04:49 +0900

    Done Base

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
+var cors = require("cors");
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(cors("*"));
 
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
 });
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings(); 
+  res.json(sightings[req.params.sightingIndex])
+})
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
