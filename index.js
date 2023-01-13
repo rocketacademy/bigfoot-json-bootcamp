6c135e9dc9c16fc62a5af868ed06aed4cde06b2e
Cljwen <carmenlaujwn@gmail.com>
2023-01-14 00:37:22 +0800

    base version

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
+
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  console.log(req.params.sightingIndex);
+  res.json(sightings[+req.params.sightingIndex]);
 });
 
 app.listen(PORT, () => {
