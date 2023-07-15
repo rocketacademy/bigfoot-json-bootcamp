163285ff098a0cc022951d9a6f91ec89d4980815
Huang Yu Hao <yuhao.huang@ntu.edu.sg>
2023-07-15 12:01:26 +0800

    added cors and sightingIndex return

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
+
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  const sightingIndex = req.params.sightingIndex;
+
+  if (sightingIndex >= 0 && sightingIndex < sightings.length) {
+    const selectedSighting = sightings[sightingIndex];
+    res.json(selectedSighting);
+  } else {
+    res.status(404).json({ error: "Sighting not found" });
+  }
 });
 
 app.listen(PORT, () => {
