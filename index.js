eb95ae273fb6c0515769736692b6b0dbeb247721
Huang Yu Hao <yuhao.huang@ntu.edu.sg>
2023-07-18 21:11:33 +0800

    added filter for backend

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ app.get("/sightings/:sightingIndex", async (req, res) => {
   }
 });
 
+app.get("/sightings/year/:Year", async (req, res) => {
+  const sightings = await getSightings();
+  const year = req.params.Year;
+  const selectedSightings = [];
+
+  sightings.forEach((sighting) => {
+    if (sighting.YEAR === year) {
+      selectedSightings.push(sighting);
+    }
+  });
+
+  if (selectedSightings !== []) {
+    res.json(selectedSightings);
+  } else {
+    res.status(404).json({ error: "No sightings in this year" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
 });
