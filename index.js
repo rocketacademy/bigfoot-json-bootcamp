25838bdd87b8bf1c8851b827c6d6cbd6181ff614
dexterch91 <dexterchewxh@hotmail.sg>
2023-07-18 23:02:15 +0800

    Added sightings index params

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 const app = express();
 
+// Enable CORS access to this server
+app.use(cors());
+
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  res.json(sightings[req.params.sightingIndex]);
+  //res.send vs res.json: json more specific
 });
 
 app.listen(PORT, () => {
