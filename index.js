10d98c24b4afab42a02a3f81446eb7b8a60fdd80
NTU_COSMO <yuhao.huang@ntu.edu.sg>
2023-07-21 17:24:21 +0800

    changed logic for year display

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const cors = require("cors");
 
 const PORT = process.env.PORT;
 const app = express();
+const YEARLIST = [
+  "before_1960",
+  "1960-69",
+  "1970-79",
+  "1980-89",
+  "1990-99",
+  "2000-2009",
+  "after_2010",
+  
+];
 
 app.use(cors());
 
@@ -31,15 +41,80 @@ app.get("/sightings/year/:Year", async (req, res) => {
   const selectedSightings = [];
 
   sightings.forEach((sighting) => {
-    if (sighting.YEAR === year) {
-      selectedSightings.push(sighting);
+    let yearNum = parseInt(sighting.YEAR);
+    if (yearNum < 100 && yearNum!==NaN){
+      yearNum +=1900;
+    }
+    switch (year) {
+      case YEARLIST[0]:
+        if (yearNum < 1960) {
+          selectedSightings.push(sighting);
+        }
+        break;
+      case YEARLIST[1]:
+        if (yearNum >= 1960 && yearNum < 1970) {
+          selectedSightings.push(sighting);
+        }
+        break;
+      case YEARLIST[2]:
+        if (yearNum >= 1970 && yearNum < 1980) {
+          selectedSightings.push(sighting);
+        }
+        break;
+      case YEARLIST[3]:
+        if (yearNum >= 1980 && yearNum < 1990) {
+          selectedSightings.push(sighting);
+        }
+        break;
+      case YEARLIST[4]:
+        if (yearNum >= 1990 && yearNum < 2000) {
+          selectedSightings.push(sighting);
+        }
+        break;
+      case YEARLIST[5]:
+        if (yearNum >= 2000 && yearNum < 2010) {
+          selectedSightings.push(sighting);
+        }
+        break;
+      case YEARLIST[6]:
+        if (yearNum >= 2010) {
+          selectedSightings.push(sighting);
+        }
+        break;
+      
+      default:
+        break;
     }
   });
 
   if (selectedSightings !== []) {
     res.json(selectedSightings);
   } else {
-    res.status(404).json({ error: "No sightings in this year" });
+    res.status(404).json({ error: "No sightings in this period" });
+  }
+});
+
+app.get("/sightings/list/allyear", async (req, res) => {
+  const sightings = await getSightings();
+  const yearList = [];
+  sightings.forEach((sighting) => {
+    yearList.push(sighting.YEAR);
+  });
+  const cleanData = yearList.filter(
+    (item) => item !== null && item !== undefined
+  );
+
+  // Sort the array in ascending order
+  cleanData.sort();
+
+  // Remove duplicate elements from the array
+  const uniqueList = [...new Set(cleanData)];
+  if (yearList !== []) {
+    res.json(uniqueList);
+  } else {
+    res
+      .status(404)
+      .json({ error: "Error in getting a list of sighting years" });
   }
 });
 
