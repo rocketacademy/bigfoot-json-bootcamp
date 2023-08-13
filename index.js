b1d543e59a7c5d53037b4a294819635881a610f8
JerryTohHS <jerrytoh93@gmail.com>
2023-08-13 22:10:43 +0800

    bigfoot-json

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const cors = require("cors");
+const express = require("express");
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors());
+
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings/:sightingsIndex", async (req, res) => {
+  const sightings = await getSightings();
+  res.json(sightings[req.params.sightingsIndex]);
 });
 
 app.listen(PORT, () => {
