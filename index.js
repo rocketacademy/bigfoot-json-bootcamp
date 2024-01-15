b829f8dca6cb0ce8d65e634a48766579f4a3a418
jasperteo <jaspertzj@outlook.sg>
2024-01-15 23:43:13 +0800

    completed base backend

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+import express from "express";
+import cors from "cors";
+import getSightings from "./utils.js";
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(cors());
 
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
 });
 
-app.listen(PORT, () => {
-  console.log(`Express app listening on port ${PORT}!`);
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  const sightingIndex = req.params.sightingIndex;
+  res.json(sightings[sightingIndex]);
 });
+
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
--- utils.js
+++ utils.js
@@ -1,13 +1,8 @@
-const { readFile } = require('fs/promises')
+import { readFile } from "node:fs/promises";
 
-const getSightings = async () => {
+export default async function getSightings() {
   // Retrieve sightings.json file contents as string
   const sightings = await readFile("./sightings.json", "utf8");
   // Return sightings as an object
   return JSON.parse(sightings);
-};
-
-
-module.exports = {
-  getSightings
 }
