9916cd47a959f2447654919d83252ada92f58ba2
estney <esther.pxy@gmail.com>
2024-01-16 08:07:29 +0800

    completed base

--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
-const { readFile } = require('fs/promises')
+const { readFile } = require("fs/promises");
 
 const getSightings = async () => {
   // Retrieve sightings.json file contents as string
+  //utf-8 - coding language for the characters
   const sightings = await readFile("./sightings.json", "utf8");
   // Return sightings as an object
   return JSON.parse(sightings);
 };
 
-
 module.exports = {
-  getSightings
-}
+  getSightings,
+};
