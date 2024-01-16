067e8a4fcec74846e52871861d54066550f8c7be
Chxshire22 <akuindra22@gmail.com>
2024-01-16 22:02:12 +0800

    can query by year now but not if there's a query string

--- index.js
+++ index.js
@@ -10,15 +10,28 @@ const app = express();
 //cors middleware
 app.use(cors())
 
+//get filtered results by YEAR. 
+app.get("/sightings/filter", async(req,res)=>{
+  const sightings = await getSightings()
+  const { YEAR, SEASON, MONTH, STATE, COUNTY  } = req.query
+  console.log(YEAR)
+  let filterSightings = sightings.filter((sighting)=>sighting.YEAR == YEAR)
+  res.send(filterSightings)
+})
+
+//get all sightings list
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
+  // console.log(sightings[1].YEAR)
   res.json(sightings);
 });
 
+//get specific sighting
 app.get("/sightings/:sightingsIndex", async(req,res)=>{
   const sightings = await getSightings()
   res.send(sightings[req.params.sightingsIndex])
 })
+
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
