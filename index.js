a8d8edff02b66935265d12bfb540341395580eb8
magiicloud <hongyun_rc@outlook.com>
2024-01-31 00:53:00 +0800

    added sighting index

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { getSightings } = require("./utils.js");
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
+app.use(cors());
 
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightingIndex = req.params.sightingIndex;
+  const sightings = await getSightings();
+  const individualSighting = sightings[sightingIndex];
+  res.json(individualSighting);
 });
 
 app.listen(PORT, () => {
