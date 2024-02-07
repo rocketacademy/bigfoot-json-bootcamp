42576d31e4e985eaab7446f149e89b1f31b39eb7
Mannushka <zaimar1910@gmail.com>
2024-02-07 22:24:03 +0800

    add filter, change index to report num for individual sighting

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const express = require("express");
 const { getSightings } = require("./utils.js");
-const { getSighting } = require("./utils.js");
 require("dotenv").config();
 const cors = require("cors");
 
 const PORT = process.env.PORT;
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors());
 
 app.get("/sightings", async (req, res) => {
-  const sightings = await getSightings();
+  const state = req.query.state;
+  const year = req.query.year;
+
+  let sightings = await getSightings();
+  if (state) {
+    sightings = sightings.filter(
+      (sighting) =>
+        sighting.STATE &&
+        sighting.STATE.toLowerCase().includes(state.toLowerCase())
+    );
+  }
+
+  if (year) {
+    sightings = sightings.filter(
+      (sighting) => sighting.YEAR && sighting.YEAR.includes(year)
+    );
+  }
+
   res.json(sightings);
 });
 
-app.get("/sightings/:index", async (req, res) => {
-  const index = req.params.index;
-  const sighting = await getSighting(index);
+app.get("/sightings/:REPORT_NUMBER", async (req, res) => {
+  const sightings = await getSightings();
+  const reportNumber = req.params.REPORT_NUMBER;
+  const sighting = sightings.find(
+    (sighting) => sighting.REPORT_NUMBER === reportNumber
+  );
+
   res.json(sighting);
 });
 
