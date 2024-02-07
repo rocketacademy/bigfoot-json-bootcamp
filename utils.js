62fbab026b8a9648a61193f0fe3b29bb6d00254c
Mannushka <zaimar1910@gmail.com>
2024-02-07 22:27:18 +0800

    change back to original version of utils.js

--- utils.js
+++ utils.js
@@ -7,14 +7,6 @@ const getSightings = async () => {
   return JSON.parse(sightings);
 };
 
-const getSighting = async (index) => {
-  // Retrieve sightings.json file contents as string
-  const sightings = await readFile("./sightings.json", "utf8");
-  // Return sightings as an object
-  return JSON.parse(sightings)[index];
-};
-
 module.exports = {
   getSightings,
-  getSighting,
 };
